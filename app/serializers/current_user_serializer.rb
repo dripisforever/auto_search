52a17fb45b4e4f0678f5dae07a7f755059f1bb43
strivemag <talga.tawit@gmail.com>
2017-04-03 00:30:30 +0600

    Change the output from CurrentUserSerializer

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -1,11 +1,18 @@
 class CurrentUserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :username, :created_at, :authentication_token, :avatar_url, :post_ids
+  attributes :id, :email, :username, :created_at, :authentication_token, :avatar_url,
+             :post_ids, :attrs
 
+  has_many :posts
+  
   def authentication_token
     JsonWebToken.encode({ user_id: objec.id })
   end
 
-  def avatar_url
-    object.avatar.url
+  def attrs
+    {
+      email: object.email,
+      username: object.username,
+      avatar_url: object.avatar.url
+    }
   end
 end
