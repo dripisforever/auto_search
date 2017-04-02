4cdefaca7e44af47a34c4abaadc0d458fe011b19
strivemag <talga.tawit@gmail.com>
2017-04-02 23:56:31 +0600

    Add filter attribute to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,16 @@ class PostsController < ApplicationController
 
   def create
     post = current_user.posts.build(post_params)
+    if post.save
+      render json: post, status: 201
+    else
+      render json: { errors: post.errors.full_messages }, status: 422
+    end
   end
 
   private
 
     def post_params
-      params.permit(:photo, :caption)
+      params.permit(:photo, :caption, :filter)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401203058) do
+ActiveRecord::Schema.define(version: 20170402175341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170401203058) do
     t.string   "photo"
     t.text     "caption"
     t.integer  "user_id"
+    t.string   "filter"
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
