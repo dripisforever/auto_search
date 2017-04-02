3f325447d377986bb76e236814b43f53b8a3c474
strivemag <talga.tawit@gmail.com>
2017-04-03 01:24:02 +0600

    Add location to Post model

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,8 +1,15 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :photo_url, :filter, :caption, :created_at, :user_id
+  attributes :id, :photo_url, :filter, :caption, :created_at, :user_id, :lat_lng, :adress
 
   belongs_to :user, serializer: UserSimpleSerializer
   def photo_url
     object.photo.url
+  end
+
+  def lat_lng
+    {
+      lat: object.lat,
+      lng: object.lng
+    }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402175341) do
+ActiveRecord::Schema.define(version: 20170402192149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20170402175341) do
     t.text     "caption"
     t.integer  "user_id"
     t.string   "filter"
+    t.string   "address"
+    t.float    "lat"
+    t.float    "lng"
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
