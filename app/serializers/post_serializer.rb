816218d8715f197daaa7efcb48080aa3ca8a28ce
strivemag <talga.tawit@gmail.com>
2017-04-04 17:17:14 +0600

    Add API endpoint for fetching posts with place_id

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :photo_url, :filter, :caption, :created_at, :user_id, :lat_lng, :adress, :likes_count, :comments_count
+  attributes :id, :photo_url, :filter, :caption, :created_at, :user_id,
+             :lat_lng, :adress, :likes_count, :comments_count, :place_id
 
   belongs_to :user, serializer: UserSimpleSerializer
   has_many :comments
