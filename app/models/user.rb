7be6acbc763b667e41a739bd6bc3884da2463390
strivemag <talga.tawit@gmail.com>
2017-04-04 13:38:35 +0600

    Create relationship model and add follow/unfollow methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, source: :post
   has_many :comments,    dependent: :destroy
 
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship",
+            foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_secure_password
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false},
@@ -23,5 +31,18 @@ class User < ApplicationRecord
 
   def dislike!(post)
     like.where(post_id: post.id).destroy_all
+  end
+
+  def follow(other_user)
+    return false if self.id == other_user.id
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following_ids.include?(other_user.id)
   end
 end
