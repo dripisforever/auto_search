ae28b5d59d03c68d940bbe8b5d112947f0f87700
strivemag <talga.tawit@gmail.com>
2017-04-05 03:57:41 +0600

    Add Tag and Tagging Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,9 @@ class Post < ApplicationRecord
   has_many :likers,   through: :likes, source: :user
   has_many :comments, dependent: destroy
 
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+  
   validates :user_id, presence: true
   validates :photo,   presence: true
 
