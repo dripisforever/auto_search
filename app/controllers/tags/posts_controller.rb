12af079dcdf61b00159d0435dad8749108202dd0
strivemag <talga.tawit@gmail.com>
2017-04-05 23:24:18 +0600

    Add API endpoint for fetching posts filtered by tag name

---
+++ app/controllers/tags/posts_controller.rb
@@ -0,0 +1,18 @@
+class Tags::PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    if @tag.present?
+      posts = @tag.posts.get_page(params[:page], 9)
+      render json: posts, meta: pagination_dict, status: 200
+    else
+      render json: { errors: ["Tag #{params[:tag_name]} not found"] }, status: 422
+    end
+  end
+
+  private
+
+    def set_tag
+      @tag = Tag.find_by(name: params[:tag_name])
+    end
+end
