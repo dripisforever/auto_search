a4825c60b6fc37757cc2f6b8fbf5b43cb4ba723b
strivemag <talga.tawit@gmail.com>
2017-04-06 16:13:41 +0600

    Fix syntax in User, fixed :destro

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts,       dependent: :destroy
   has_many :likes,       dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
-  has_many :comments,    dependent: :destro
+  has_many :comments,    dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
             foreign_key: "follower_id",
             dependent: :destroy
