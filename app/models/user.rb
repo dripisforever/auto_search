a59231a9cc649c5e211885e6ffcc4b066af3f9db
strivemag <talga.tawit@gmail.com>
2017-04-09 05:02:45 +0600

    Add format validation to username in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :likes,       dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   has_many :comments,    dependent: :destroy
+
+
   has_many :active_relationships, class_name: "Relationship",
             foreign_key: "follower_id",
             dependent: :destroy
@@ -12,14 +14,19 @@ class User < ApplicationRecord
             foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
+
   has_many :notifications, dependent: :destroy, foreign_key: :recipient_id
 
   has_secure_password validations: false
+
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  USERNAME_REGEX = /\A[a-zA-Z0-9_-]{3,30}\z/
+
   validates :email, presence: true, uniqueness: {case_sensitive: false},
             format: {with: EMAIL_REGEX}, unless: :facebook_login?
 
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {in: 2..30 }
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
+            format: { with: USERNAME_REGEX, message: "should be one word"}, unless: :facebook_login?
   validates :password, presence: true, length: { minimum: 8 }, unless: :facebook_login?
 
   mount_uploader :avatar, AvatarUploader
