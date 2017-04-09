d346e5d0b2c643cb1b8c6d1937c6e0d4ee1fbf46
strivemag <talga.tawit@gmail.com>
2017-04-09 11:00:55 +0600

    Add pagination to follower and following API

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -1,10 +1,10 @@
 class Users::FollowersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
-  
+
   def index
-    followers = @user.followers
-    render json: followers, status: 200
+    followers = @user.followers.paginate(page: params[:page], per_page: 10)
+    render json: followers, meta: pagination_dict(followers), status: 200
   end
 
   private
